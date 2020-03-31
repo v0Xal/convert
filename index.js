a436d9f7ab51b83c87948f3fcc59720725b70f1b
v0Xal <58456527+v0Xal@users.noreply.github.com>
2020-03-31 10:22:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+function convertBinary() {
+  var output = document.getElementById("output");
+  var input = document.getElementById("input");
+  output = "";
+  for (var i = 0; i < input.length; i++) {
+      output += input[i].charCodeAt(0).toString(2) + " ";
+  }
+}
+
 function convert(){
-
-
-
+let input = document.getElementById("input");
+let type  = document.getElementById("type").value;
+let output;
+if (type = base64){
+ output = btoa(input); 
+  document.getElementById("output") = output;
+} else{
+  output = convertBinary(input);
+  document.getElementById("output") = output;
+}
 }
